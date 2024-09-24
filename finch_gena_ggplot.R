959f7e5315dfaa2b9a8abe273f383977ae57c25c
GenaSandoval <sandovalgena@gmail.com>
2024-09-24 10:38:08 -0700

    please work

--- finch_gena_ggplot.R
+++ finch_gena_ggplot.R
@@ -4,3 +4,4 @@ library(dslabs)
 
 ###Simplifying dataset -Gena
 finch_simple = subset(finch, select = -c(month, day, year, day_of_year, number_of_visit, latitude, longitude, wind, pox_scale, pox_lesion))
+
